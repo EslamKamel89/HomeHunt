64efd99b34783f3af6b534f4879a2bfd5f4fc6c4
EslamKamel89 <elsamkamelforex@gmail.com>
2025-02-06 20:16:06 +0200

    Listing filters markup and styling

---
+++ resources/js/Pages/Listing/Components/Filters.vue
@@ -0,0 +1,57 @@
+<template>
+    <form>
+        <div class="mb-8 mt-4 flex flex-wrap items-center justify-start gap-2">
+            <div class="flex flex-nowrap items-center">
+                <input
+                    type="number"
+                    class="w-[100px] rounded-s-md"
+                    placeholder="Price from"
+                />
+                <input
+                    type="number"
+                    class="w-[100px] rounded-e-md"
+                    placeholder="Price to"
+                />
+            </div>
+            <div class="flex flex-nowrap items-center">
+                <select class="w-[100px] rounded-s-md">
+                    <option :value="null">Beds</option>
+                    <option v-for="n in 5" :value="n" :key="n">{{ n }}</option>
+                    <option :value="6">6+</option>
+                </select>
+                <select class="w-[100px] rounded-e-md">
+                    <option :value="null">Baths</option>
+                    <option v-for="n in 5" :value="n" :key="n">{{ n }}</option>
+                    <option :value="6">6+</option>
+                </select>
+            </div>
+            <div class="flex flex-nowrap items-center">
+                <input
+                    type="number"
+                    class="w-[100px] rounded-s-md"
+                    placeholder="Area from"
+                />
+                <input
+                    type="number"
+                    class="w-[100px] rounded-e-md"
+                    placeholder="Area to"
+                />
+            </div>
+            <div class="flex flex-nowrap">
+                <button
+                    type="submit"
+                    class="text-whte w-[100px] rounded-s-md border border-blue-500 bg-blue-500 py-2 text-white"
+                >
+                    Find
+                </button>
+                <button
+                    type="reset"
+                    class="w-[100px] rounded-e-md border border-red-500 text-red-700"
+                >
+                    Clear
+                </button>
+            </div>
+        </div>
+    </form>
+</template>
+<script lang="ts" setup></script>
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -11,6 +11,7 @@
     <template v-if="error">
         <Alert :isError="true" :content="error ?? 'Error'"></Alert>
     </template>
+    <Filters />
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         <ListingItem
             v-for="listing in listings.data"
@@ -19,8 +20,8 @@
             :listing="listing"
         >
         </ListingItem>
-        <PaginationComp :links="listings.links" />
     </div>
+    <PaginationComp :links="listings.links" />
 </template>
 
 <script lang="ts" setup>
@@ -31,6 +32,7 @@ import MainLayout from '@/Layouts/MainLayout.vue';
 import { Listing, Pagination } from '@/types/types';
 import { usePage } from '@inertiajs/vue3';
 import { computed } from 'vue';
+import Filters from './Components/Filters.vue';
 defineProps<{
     listings: Pagination<Listing>;
 }>();
